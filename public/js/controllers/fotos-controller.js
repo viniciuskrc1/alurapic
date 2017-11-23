b81a02edf18cac5081cfde9b7b977cf9ddbb5e5b
DESKTOP-447JECE\vinicius <viniciuskrc1@gmail.com>
2017-11-23 11:18:34 -0200

    Criado primeiro controller do projeto.
    //

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,8 @@
+angular.module('alurapic').controller('FotosController', function($scope) {
+
+    $scope.foto = {
+      titulo: 'Leao',
+      url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
+    };
+
+});
